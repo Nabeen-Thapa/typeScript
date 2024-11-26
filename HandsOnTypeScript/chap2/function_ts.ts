0c793f3283a5ed7df76cb5dc457a93bff6eb2658
Nabeen-Thapa <nabeen2058@gmail.com>
2024-11-26 22:55:10 +0545

    add ts function

--- HandsOnTypeScript/chap2/function_ts.ts
+++ HandsOnTypeScript/chap2/function_ts.ts
@@ -8,7 +8,7 @@ console.log(square(5));//25
 
 //arrow function-ES6
 
-const cube = (n:number) =>{
+const cube = (n:number, n1?:number) =>{ //n1? mens n1 is optionl
     return n*n *n;
 }
 console.log(cube(3)); //27
