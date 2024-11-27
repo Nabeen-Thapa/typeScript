87da0dd7cc52e6d48111c291917ee821d3758b45
Nabeen-Thapa <nabeen2058@gmail.com>
2024-11-27 10:21:35 +0545

    add ts functions

--- HandsOnTypeScript/chap2/function_ts.js
+++ HandsOnTypeScript/chap2/function_ts.js
@@ -4,7 +4,18 @@ function square(num) {
 }
 console.log(square(5)); //25
 //arrow function-ES6
-var cube = function (n) {
+var cube = function (n, n1) {
     return n * n * n;
 };
-console.log(cube(3));
+console.log(cube(3)); //27
+var logMessage = function (msg) {
+    console.log("this is ".concat(msg));
+};
+logMessage('typescript function'); //output: this is typescript function
+//If we want to declare a function variable, but not define it 
+//// Declare the varible sayHello, and give it a function signature that takes a string and returns nothing.
+var sayHello;
+sayHello = function (name) {
+    console.log("hello ".concat(name));
+};
+sayHello("Ram");
--- HandsOnTypeScript/chap2/function_ts.ts
+++ HandsOnTypeScript/chap2/function_ts.ts
@@ -8,7 +8,23 @@ console.log(square(5));//25
 
 //arrow function-ES6
 
-const cube = (n:number, n1?:number) =>{ //n1? mens n1 is optionl
+const cube = (n:number, n1?:number) =>{ //n1? mens n1 is optional
     return n*n *n;
 }
 console.log(cube(3)); //27
+
+
+const logMessage = (msg:string)=>{
+    console.log(`this is ${msg}`);
+}
+logMessage('typescript function');//output: this is typescript function
+
+
+
+//If we want to declare a function variable, but not define it 
+//// Declare the varible sayHello, and give it a function signature that takes a string and returns nothing.
+let sayHello: (name:string) =>void;
+sayHello = (name)=>{
+    console.log(`hello ${name}`);
+}
+sayHello("Ram");
