3d702d9d44edc51c393278ba219ef179feeb7428
Nabeen-Thapa <nabeen2058@gmail.com>
2024-11-28 17:07:48 +0545

    add never type

---
+++ HandsOnTypeScript/chap2/never-type.js
@@ -0,0 +1,11 @@
+//A never type is used to indicate a function that never returns (completes), or a variable that is not set to anything, not even null.
+function oldEnough(age) {
+    if (age > 59) {
+        throw Error("too old");
+    }
+    if (age <= 18) {
+        return false;
+    }
+    return true;
+}
+console.log(oldEnough(80));
